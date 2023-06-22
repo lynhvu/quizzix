8949f2b67d35f120ffe3145c1e2e2a707c097615
Linh Vu <linhvu.138@utexas.edu>
2023-06-22 16:43:03 -0400

    semi finished splash front-end

--- quizzix/src/components/Splash.js
+++ quizzix/src/components/Splash.js
@@ -1,31 +1,53 @@
-import splashbg from '../styles/splash/splashbg.jpeg'
-import logo from '../styles/logo-nobg.png'
-import styles from '../styles/splash/styles.css'
+import logo from "../styles/logo-nobg.png";
+import "../styles/splash/styles.css";
 
 function Splash() {
-    return (
-        <div className="container bg">
-            <div className="text-center" id="splash-content">
+  return (
+    <div className="container bg">
+      <div className="text-center" id="splash-content">
+        {/* logo */}
+        <img
+          src={logo}
+          alt="Quizzix logo"
+          className="mb-4"
+          width="150"
+          height="150"
+          id="logo"
+        />
 
-                {/* logo */}
-                <img src={logo} alt="Quizzix logo" width="100" height="100" id="logo"/>
+        {/* title */}
+        <h1 className="display-5 fw-bold white-text">Quizzix</h1>
 
-                {/* title */}
-                <h1 className="display-5 fw-bold white-text">Quizzix</h1>
-
-                {/* intro */}
-                <div className="col-lg-6 mx-auto">
-                    <p className="lead mb-4 white-text">Ignite your intellect and embrace the quizzing adventure!</p>
-
-                    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-
-                        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-                    </div>
-                </div>
-            </div>
+        {/* intro */}
+        <div className="mx-auto">
+          <p className="lead mb-4 white-text">
+            Ignite your intellect and embrace the quizzing adventure!
+          </p>
         </div>
-    )
+
+        {/* search bar */}
+        <div>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="col-sm-12"
+              id="input-form"
+              placeholder="Enter a topic"
+            />
+          </div>
+        </div>
+
+        {/* buttons */}
+        <div class="d-grid gap-2 d-flex justify-content-center">
+          <button type="submit" className="btn-md px-4 button">
+            <span class="text-container">
+              <span class="text">Start</span>
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Splash;
