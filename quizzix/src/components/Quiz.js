4c9c7f6a6d3555cb3a2a66b2ea7645eb2a9c71d6
Linh Vu <linhvu.138@utexas.edu>
2023-06-26 21:09:17 -0400

    made buttons for answers

--- quizzix/src/components/Quiz.js
+++ quizzix/src/components/Quiz.js
@@ -8,7 +8,10 @@ function Quiz() {
           {/* question */}
           <div id="question-field">
             <h1>Question 1 </h1>
-            <p id="question-text">Which company did Valve cooperate with in the creation of the Vive?</p>
+            <p id="question-text">
+              Which company did Valve cooperate with in the creation of the
+              Vive?
+            </p>
           </div>
         </div>
 
@@ -16,10 +19,10 @@ function Quiz() {
 
         <div className="row">
           <div className="grid-container">
-            <div className="grid-item">A</div>
-            <div className="grid-item">B</div>
-            <div className="grid-item">C</div>
-            <div className="grid-item">D</div>
+            <button className="grid-item">A</button>
+            <button className="grid-item">B</button>
+            <button className="grid-item">C</button>
+            <button className="grid-item">D</button>
           </div>
         </div>
       </div>
